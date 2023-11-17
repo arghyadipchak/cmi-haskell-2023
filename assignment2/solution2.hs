faba4b12cd480c09c175bd04ac8f944edc1d4fed
Arghyadip Chakraborty <arghyadip.chak16@gmail.com>
2023-11-17 22:00:54 +0530

    feat(ass2): added assignment 2

---
+++ assignment2/solution2.hs
@@ -0,0 +1,103 @@
+import Data.Int (Int8)
+import Data.List (foldl')
+import Data.Ratio (denominator, numerator, (%))
+import Data.Word (Word8)
+
+-- Problem 1
+-- Check if input list is an AP. Length <= 2 means it is a trivial AP.
+isAP :: [Integer] -> Bool
+isAP [] = True
+isAP [x] = True
+isAP [x, y] = True
+isAP (x : y : xs) = isAP_ (y - x) (y : xs)
+  where
+    isAP_ :: Integer -> [Integer] -> Bool
+    isAP_ z [x] = True
+    isAP_ z (x : y : xs)
+      | y - x == z = isAP_ z (y : xs)
+      | otherwise = False
+
+-- Problem 2
+-- targetSum t l checks if two elements at distinct positions of l sum to t
+targetSum :: Integer -> [Integer] -> Bool
+targetSum _ [] = False
+targetSum _ [x] = False
+targetSum t (x : xs) = targetSum_ xs || targetSum t xs
+  where
+    targetSum_ :: [Integer] -> Bool
+    targetSum_ (y : ys) = (x + y == t) || targetSum_ ys
+
+-- Problem 3
+-- increment a list of Word8-s representing an integer
+incr :: [Word8] -> [Word8]
+incr [] = [1]
+incr l = map fromInteger (if t then 1 : l' else l')
+  where
+    (l', t) = incr_ (map toInteger l)
+    incr_ :: [Integer] -> ([Integer], Bool)
+    incr_ [255] = ([0], True)
+    incr_ [x] = ([x + 1], False)
+    incr_ (x : xs)
+      | z = if x == 255 then (0 : y, True) else ((x + 1) : y, False)
+      | otherwise = (x : y, False)
+      where
+        (y, z) = incr_ xs
+
+-- Problem 4
+-- Find Int8 value represented by list of 8 bits (in twos complement form), and find twos complement representation (consisting of 8 bits) of an Int8.
+twosVal :: [Bool] -> Int8
+twosVal (x : xs) = if x then if v then -u else -z else z
+  where
+    (z, u, v) = twosVal_ xs
+    twosVal_ :: [Bool] -> (Int8, Int8, Bool)
+    twosVal_ [y] = (if y then 1 else 0, 2, x && not y)
+    twosVal_ (y : ys) = (a + if not c && x then (if y then 0 else b) else (if y then b else 0), 2 * b, c && not y)
+      where
+        (a, b, c) = twosVal_ ys
+
+twosRep :: Int8 -> [Bool]
+twosRep x = t : z
+  where
+    t = x < 0
+    (z, _, _) = twosRep_ 7
+    twosRep_ :: Int -> ([Bool], Int8, Bool)
+    twosRep_ 1 = ([f], d, t && not f)
+      where
+        (d, e) = divMod (if t then -x else x) 2
+        f = e == 1
+    twosRep_ n = (((not c && t) /= f) : a, d, c && not f)
+      where
+        (a, b, c) = twosRep_ $ n - 1
+        (d, e) = divMod b 2
+        f = e == 1
+
+-- Problem 5
+-- Compute a finite continued fraction representing a rational, and the rational represented by a finite continued fraction.
+invert :: Rational -> Rational
+invert x = denominator x % numerator x
+
+cf :: Rational -> [Integer]
+cf 0 = [0]
+cf r = n : if r == n % 1 then [] else cf $ invert (r - n % 1)
+  where
+    n = div (numerator r) (denominator r)
+
+computeRat :: [Integer] -> Rational
+computeRat [] = 0
+computeRat [x] = x % 1
+computeRat (x : xs) = (x * denominator y + numerator y) % denominator y
+  where
+    y = invert $ computeRat xs
+
+-- Problem 6
+-- Compute approximations of sqrt 15, based on infinite continued fraction.
+computeFrac :: Rational -> Double
+computeFrac x = fromIntegral (numerator x) / fromIntegral (denominator x)
+
+approxTarget :: Double -> Rational
+approxTarget eps = approxTarget_ [3]
+  where
+    sq15 = sqrt 15
+    approxTarget_ l = if abs (sq15 - computeFrac cfl) < eps then cfl else approxTarget_ $ 3 : 1 : 6 : tail l
+      where
+        cfl = computeRat l
---
+++ assignment2/testcases3.hs
@@ -0,0 +1,60 @@
+-- Problem 1
+isAP []
+isAP [-13]
+isAP [-10,-143]
+isAP [16,-2,-20,-38,-56,-74,-92]
+not $ isAP [2,20,37,56,74,92]
+
+-- Problem 2
+not $ targetSum 20 [1..10]
+not $ targetSum 20 [10]
+targetSum 20 [-13,33]
+targetSum 20 ([-1,-2,-3,-4,-5]++[10]++[6..10])
+targetSum 20 ([-1,-2,-3,-4,-5]++[33]++[6..9]++[-13])
+
+-- Problem 3
+incr [] == [1]
+incr [0] == [1]
+incr [255,255,255,254,255,255,255,255] == [255,255,255,255,0,0,0,0]
+incr [255,255,255,255,255,255,255,255] == [1,0,0,0,0,0,0,0,0]
+incr [255] == [1,0]
+
+-- Problem 4
+twosVal [False, False, False, False, False, False, False, False] == 0
+twosVal [False, False, False, False, False, False, False, True] == 1
+twosVal [False, True, True, False, True, False, True, False] == 106
+twosVal [True, False, False, True, True, False, True, False] == -102
+twosVal [True, True, True, False, True, False, True, False] == -22
+
+twosRep 0 == [False, False, False, False, False, False, False, False]
+twosRep 1 == [False, False, False, False, False, False, False, True]
+twosRep 106 == [False, True, True, False, True, False, True, False]
+twosRep (-102) == [True, False, False, True, True, False, True, False]
+twosRep (-22) == [True, True, True, False, True, False, True, False]
+
+-- Problem 5
+computeRat [33,957,997,977,763] == 23472285165790 % 711258846951
+computeRat (replicate 100 1) == 573147844013817084101 % 354224848179261915075
+computeRat [25,24..1] == 33973805081805192501772225 % 1356695125590833848455793
+computeRat [12,3,43,2564,3,332243] == 4097221463699 % 332276225380
+computeRat [1..100] == 210628326606030925509504995396115751215082237173770949560136689651065020446811215535403721603460152269102594085067245222090841983806373539433329769060736986251 % 146971108555054591263734471078160507352050045572198741030186489794108431298591736983812285328810821610957278981960240168856854819813194627616076643581220741550
+
+cf (23472285165790 % 711258846951) == [33,957,997,977,763]
+cf (573147844013817084101 % 354224848179261915075) == replicate 98 1 ++ [2]
+cf (33973805081805192501772225 % 1356695125590833848455793) == [25,24..3] ++ [3]
+cf (4097221463699 % 332276225380) == [12,3,43,2564,3,332243]
+cf (210628326606030925509504995396115751215082237173770949560136689651065020446811215535403721603460152269102594085067245222090841983806373539433329769060736986251 % 146971108555054591263734471078160507352050045572198741030186489794108431298591736983812285328810821610957278981960240168856854819813194627616076643581220741550) == [1..100]
+
+-- Problem 6
+checkApproxTarget :: Double -> Bool
+checkApproxTarget e = abs (sq15 - computeFrac (approxTarget e)) < e
+  where
+    sq15 = sqrt 15
+    computeFrac :: Rational -> Double
+    computeFrac x = fromIntegral (numerator x) / fromIntegral (denominator x)
+
+checkApproxTarget 1
+checkApproxTarget 10
+checkApproxTarget 0.0001
+checkApproxTarget 0.0000000001
+checkApproxTarget 0.00000000000000001
