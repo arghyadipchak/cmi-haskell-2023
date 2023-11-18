999bd55af263b62c69cd6fac08432c33c58689fe
Arghyadip Chakraborty <arghyadip.chak16@gmail.com>
2023-11-18 10:26:00 +0530

    feat(ass3): added assignment 3

---
+++ assignment3/template3.hs
@@ -0,0 +1,22 @@
+-- Problem 1
+hamming :: Eq a => [a] -> [a] -> Maybe Int
+hamming xs ys = undefined
+
+-- Problem 2
+parity :: Int -> Bool
+parity n = undefined
+
+-- Problem 3
+mss :: [Integer] -> (Integer, [Integer])
+mss xs = undefined
+
+-- Problem 4
+squares :: [(Int,Int)]
+squares = [(x,y) | x <- [0..7], y <- [0..7]]
+
+knightMove :: (Int,Int) -> Int -> [(Int,Int)]
+knightMove (x,y) n = undefined
+
+-- Problem 5
+editDistance :: String -> String -> (Int,String)
+editDistance as bs = undefined
